ccb9ee48a0ef683f59bda14f122810609c40110e
pranaychinde936 <pranaychinde936@gmail.com>
2025-01-29 22:37:24 +0530

    Time: 3 ms (46.34%), Space: 8.6 MB (42.41%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0)           return false;
+
+        int temp = x;
+        long long ans = 0;
+        while(temp > 0){
+            int dig = temp % 10;
+            temp /= 10;
+            // if(ans > INT_MAX/10)             return false;
+            ans = ans * 10 + dig;
+        }
+
+        if(ans > INT_MAX)               return false;
+
+        return (int)ans == x;
+    }
+};
