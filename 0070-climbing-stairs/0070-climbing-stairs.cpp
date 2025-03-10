98f980f9718a20006328129b32ea1fed4b7d0b37
pranaychinde936 <pranaychinde936@gmail.com>
2025-03-10 23:25:10 +0530

    Time: 0 ms (100%), Space: 8.7 MB (17.18%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+private:
+    int solve(int n, vector<int> &dp){
+        // base case
+        if(n == 0)          return 1;
+        if(n < 0)           return 0;
+
+        if(dp[n] != -1)     return dp[n];
+
+        return dp[n] = solve(n-1, dp) + solve(n-2, dp);
+    }
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1, -1);
+        return solve(n, dp);
+    }
+};
