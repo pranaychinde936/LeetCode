8f63bdaf3b42d47f95c14d30a077f68550f8b569
pranaychinde936 <pranaychinde936@gmail.com>
2025-05-27 23:22:20 +0530

    Time: 0 ms (100%), Space: 7.7 MB (85.49%) - LeetHub

---
+++ 3371-harshad-number/3371-harshad-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum = 0;
+        int temp = x;
+
+        while(x){
+            sum += x % 10;
+            x /= 10;
+        }
+
+        return temp % sum == 0 ? sum : -1;
+    }
+};
