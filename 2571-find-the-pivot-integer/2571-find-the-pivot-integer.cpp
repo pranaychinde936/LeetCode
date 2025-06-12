1d6b748aa11904a308342841ecaf4d100759d993
pranaychinde936 <pranaychinde936@gmail.com>
2025-06-12 23:07:38 +0530

    Time: 471 ms (5.45%), Space: 8.2 MB (17.55%) - LeetHub

---
+++ 2571-find-the-pivot-integer/2571-find-the-pivot-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int lsum = 0, rsum = (n*(n+1))/2;
+        
+        int i=0;
+        while(lsum <= rsum){
+            if(lsum == rsum-i)        return i;
+            lsum += i;
+            rsum -= i;
+            i++;
+            cout<< lsum<< "  "<< rsum<< endl;
+        }
+
+        return -1;
+    }
+};
