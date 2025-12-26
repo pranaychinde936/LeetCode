3fa5a74ee4b728674338db0883f7963b501b5865
pranaychinde936 <pranaychinde936@gmail.com>
2025-12-27 00:13:42 +0530

    Time: 0 ms (100%), Space: 17.2 MB (96.7%) - LeetHub

---
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
+        ans = []
+
+        for i, num in enumerate(nums):
+            if num == target:
+                ans.append(i)
+            elif len(ans) > 0:
+                break
+        
+        return ans
