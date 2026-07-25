9844dce44072e9709433b628b87ca3a313b16a00
pranaychinde936 <pranaychinde936@gmail.com>
2026-07-25 11:27:31 +0530

    Time: 1 ms (97.11%), Space: 44.8 MB (78.74%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int ans = 0;
+
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int i = 0, j = 0;
+        int m = g.size(), n = s.size();
+
+        while(i < m && j < n){
+            if(s[j] >= g[i]){
+                ans = ans + 1;
+                i++;
+            }
+            j++;
+        }
+
+        return ans;
+    }
+};
